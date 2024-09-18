7ee8d705a11946bc954b32ac8476a40d18b16bc1
Dale Morris <dalemorris2021@gmail.com>
2024-09-18 02:44:45 -0400

    Redirected Main to Run method

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,10 @@
 ﻿namespace Risk {
     public class App {
         public static void Main(String[] args) {
+            (new App()).Run();
+        }
+
+        public void Run() {
             Console.WriteLine("Hello, World!");
         }
     }
