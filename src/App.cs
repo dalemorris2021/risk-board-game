e632397a9cb3d57878a8f3132e1179c8367a06e6
Dale Morris <dalemorris2021@gmail.com>
2024-09-18 03:00:52 -0400

    Renamed Program.cs to App.cs

---
+++ src/App.cs
@@ -0,0 +1,11 @@
+﻿namespace Risk {
+    public class App {
+        public static void Main(String[] args) {
+            (new App()).Run();
+        }
+
+        public void Run() {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
