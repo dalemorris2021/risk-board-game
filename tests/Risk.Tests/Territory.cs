5a63d84524b8eca5c32f83e61de6ba7dfbe3615a
Dale Morris <dalemorris2021@gmail.com>
2024-09-30 23:54:13 -0400

    Added tests for constructor, getters, and setters

--- tests/Risk.Tests/Territory.cs
+++ tests/Risk.Tests/Territory.cs
@@ -1,4 +1,5 @@
-using Risk;
+using System.Collections;
+using System.Drawing;
 
 namespace Risk.Tests {
     public class Territory {
@@ -14,6 +15,39 @@ namespace Risk.Tests {
             string name = t1.Name;
 
             Assert.Equal(territoryName, name);
+        }
+
+        [Fact]
+        public void OccupyingPlayerShouldBeNull() {
+            Risk.Territory t1 = new Risk.Territory("");
+
+            Assert.Null(t1.OccupyingPlayer);
+        }
+
+        [Theory]
+        [ClassData(typeof(PlayerData))]
+        public void OccupyingPlayerShouldBeAsGiven(Player occupyingPlayer) {
+            Risk.Territory t1 = new Risk.Territory("");
+
+            t1.OccupyingPlayer = occupyingPlayer;
+
+            Assert.Equivalent(occupyingPlayer, t1.OccupyingPlayer);
+        }
+
+        public class PlayerData : IEnumerable<object[]> {
+            public IEnumerator<object[]> GetEnumerator() {
+                yield return new object[] { new Player("John", Color.Black) };
+                yield return new object[] { new Player("", Color.Blue) };
+                yield return new object[] { new Player("Mark Brown", Color.Red) };
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        [Fact]
+        public void ArmySizeShouldBeZero() {
+            Risk.Territory t1 = new Risk.Territory("");
+
+            Assert.Equal(0, t1.ArmySize);
         }
     }
 }
