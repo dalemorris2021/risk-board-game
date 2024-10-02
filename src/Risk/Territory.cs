a416f5e0cd697a6682b2233cab6073bc890d76b8
Dale Morris <dalemorris2021@gmail.com>
2024-10-02 13:59:52 -0400

    Modified access to mutation of ArmySize

--- src/Risk/Territory.cs
+++ src/Risk/Territory.cs
@@ -2,12 +2,23 @@ namespace Risk {
     public class Territory {
         public string Name { get; }
         public Player OccupyingPlayer { get; set; }
-        public int ArmySize { get; set; }
+        public uint ArmySize { get; private set; }
 
         public Territory(string name) {
             Name = name;
             OccupyingPlayer = null;
             ArmySize = 0;
+        }
+
+        public void IncreaseArmySize(uint size) {
+            ArmySize += size;
+        }
+
+        public void DecreaseArmySize(uint size) {
+            if (size > ArmySize) {
+                throw new OverflowException("ArmySize overflowed");
+            }
+            ArmySize -= size;
         }
     }
 }
