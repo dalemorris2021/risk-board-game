5a7fa08a7b4a8b44527470003e0da0390adc325b
Dale Morris <dalemorris2021@gmail.com>
2024-10-11 15:08:02 -0400

    Added YAML support and graph capabilities

--- src/Risk/Game.cs
+++ src/Risk/Game.cs
@@ -1,7 +1,30 @@
-namespace Risk {
-    public class Game {
-        public void Run() {
-            Console.WriteLine("Hello from Game!");
-        }
+namespace Risk;
+
+using QuikGraph;
+using QuikGraph.Collections;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+public class Game {
+    public void Run() {
+        Territory Alaska = new Territory("Alaska");
+        Territory NorthwestTerritory = new Territory("Northwest Territory");
+        Territory Ontario = new Territory("Ontario");
+
+        var neighbors = new SEdge<Territory>[] {
+            new SEdge<Territory>(Alaska, NorthwestTerritory),
+            new SEdge<Territory>(NorthwestTerritory, Ontario),
+        };
+        var terrs = neighbors.ToUndirectedGraph<Territory, SEdge<Territory>>(false);
+
+        ISerializer serializer = new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+        string yaml = "---\n"
+                + serializer.Serialize(terrs.Vertices)
+                + "---\n"
+                + serializer.Serialize(terrs.Edges);
+
+        Console.WriteLine(yaml);
     }
 }
--- src/Risk/Territory.cs
+++ src/Risk/Territory.cs
@@ -1,24 +1,9 @@
 namespace Risk {
     public class Territory {
         public string Name { get; }
-        public Player OccupyingPlayer { get; set; }
-        public uint ArmySize { get; private set; }
 
         public Territory(string name) {
             Name = name;
-            OccupyingPlayer = null;
-            ArmySize = 0;
-        }
-
-        public void IncreaseArmySize(uint size) {
-            ArmySize += size;
-        }
-
-        public void DecreaseArmySize(uint size) {
-            if (size > ArmySize) {
-                throw new OverflowException("ArmySize overflowed");
-            }
-            ArmySize -= size;
         }
     }
 }
