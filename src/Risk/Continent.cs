fa48b2883a8fec475313237499bf23c1747185d6
Dale Morris <dalemorris2021@gmail.com>
2024-10-14 12:36:31 -0400

    Removed commented-out code

--- src/Risk/Continent.cs
+++ src/Risk/Continent.cs
@@ -1,33 +1,3 @@
 namespace Risk;
 
-/*using System.Diagnostics.CodeAnalysis;
-using System.Text.Json;
-using System.Text.Json.Serialization;*/
-
 public record Continent(string Name, uint ArmyBonus, ICollection<Territory> Territories);
-
-/*public sealed class ContinentJsonConverter : JsonConverter<Continent> {
-    public override Continent Read(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options) =>
-        new Continent(reader.GetString(), reader.GetUInt32(), reader.Get);
-
-    public override void Write(
-            Utf8JsonWriter writer,
-            Continent continent,
-            JsonSerializerOptions options) =>
-        writer.WriteStringValue(continent.Name);
-
-    public override Continent ReadAsPropertyName(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options) =>
-        Read(ref reader, typeToConvert, options);
-
-    public override void WriteAsPropertyName(
-            Utf8JsonWriter writer,
-            [DisallowNull] Continent territory,
-            JsonSerializerOptions options) =>
-        writer.WritePropertyName(territory.Name);
-}*/
