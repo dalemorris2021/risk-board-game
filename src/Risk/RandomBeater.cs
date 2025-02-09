1adfb8eda3168ef25fa4180133b4161db6c949dc
Dale Morris <dalemorris2021@gmail.com>
2025-02-08 20:14:48 -0500

    Added RandomBeater.cs

---
+++ src/Risk/RandomBeater.cs
@@ -0,0 +1,15 @@
+using System.Drawing;
+
+namespace Risk;
+
+public class RandomBeater : IPlayer {
+    public string Name { get; set; } = "Random";
+    public IEnumerable<Card> Cards { get; set; } = [];
+    public int NumArmies { get; set; }
+    public int NumTerritoriesOwned { get; set; }
+    public Color Color { get; set; }
+
+    public void TakeTurn(Game game) {
+        game.TakeAction(game.Actions[0]);
+    }
+}
