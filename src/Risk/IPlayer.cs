232a84e540954a9407c72578cbf0ab2bae9c2c28
Dale Morris <dalemorris2021@gmail.com>
2025-04-05 20:32:48 -0400

    Modified interface to support potential TCP connections

--- src/Risk/IPlayer.cs
+++ src/Risk/IPlayer.cs
@@ -1,9 +1,7 @@
-using System.Drawing;
-
 namespace Risk;
 
-public interface IPlayer {
-    public string Name { get; set; }
-    public Color Color { get; set; }
-    public void TakeTurn(Game game);
+public interface IPlayer
+{
+    void SendActions(IList<Action> actions);
+    Action ReceiveAction();
 }
